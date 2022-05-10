549f129f1f8d0a893d1abebbde88fcd29ccc26ac
Darshan Aswani <darshan.aswani64@gmail.com>
2022-05-10 11:13:56 +0530

    prepared for deployment

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,15 @@ const {
   deleteProduct,
   createProduct,
   updateProduct,
+  createdProductReview,
 } = require('../controllers/productControllers');
 const { admin, protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+
+router.route('/:id/reviews').post(protect, createdProductReview);
 
 router
   .route('/:id')
--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,20 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes.js');
 const uploadRoutes = require('./routes/uploadRoutes');
+const morgan = require('morgan');
 
 const cors = require('cors');
 
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(cors());
 
 connectDB();
-
-app.get('/', (req, res) => {
-  console.log('root path');
-  res.send('Api is running');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -33,6 +33,18 @@ app.get('/api/config/paypal', (req, res) => {
 app.use('/api/upload', uploadRoutes);
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/../frontend/build')))
+
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+  })
+}else{
+  app.get('/', (req, res) => {
+    res.send('Api is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
