cc95247a67730efdc2b943b9b9e865ce78ee655c
Darshan Aswani <darshanaswani63@gmail.com>
2022-11-18 12:04:12 +0530

    docker updated

---
+++ backend/backend/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const path = require("path");
+const productRoutes = require("./routes/productRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes.js");
+const uploadRoutes = require("./routes/uploadRoutes");
+const morgan = require("morgan");
+
+const cors = require("cors");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+app.use("/api/upload", uploadRoutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
